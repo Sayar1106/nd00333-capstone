741da4be3a7879be5a4061933cab5101f58807a3
Sayar Banerjee <30318719+Sayar1106@users.noreply.github.com>
2021-01-10 12:52:27 +0000

    Add files via upload

---
+++ starter_file/endpoint.py
@@ -0,0 +1,49 @@
+import requests
+import json
+
+# URL for the web service, should be similar to:
+# 'http://8530a665-66f3-49c8-a953-b82a2d312917.eastus.azurecontainer.io/score'
+scoring_uri = "http://30edd853-7487-4e6b-be16-ff4dd8e659d2.southcentralus.azurecontainer.io/score"
+
+# Two sets of data to score, so we get two results back
+data = {"data":
+        [
+          {
+            "city_development_index": 0.899,
+            "gender": "Male",
+            "relevent_experience": "No relevent experience",
+            "enrolled_university": "Part time course",
+            "education_level": "Masters",
+            "major_discipline": "STEM",
+            "experience": 10,
+            "company_size": "50-99",
+            "company_type": "Pvt Ltd",
+            "last_new_job": 2,
+            "training_hours": 12
+          },
+          {
+            "city_development_index": 0.665,
+            "gender": "Female",
+            "relevent_experience": "Has relevent experience",
+            "enrolled_university": "no_enrollment",
+            "education_level": "Graduate",
+            "major_discipline": "STEM",
+            "experience": 18,
+            "company_size": "100-500",
+            "company_type": "Pvt Ltd",
+            "last_new_job": 4,
+            "training_hours": 43
+          },
+      ]
+    }
+# Convert to JSON string
+input_data = json.dumps(data)
+with open("data.json", "w") as _f:
+    _f.write(input_data)
+
+# Set the content type
+headers = {"Content-Type": "application/json"}
+
+# Make the request and display the response
+resp = requests.post(scoring_uri, input_data, headers=headers)
+print(resp.json())
---
+++ starter_file/train.py
@@ -0,0 +1,85 @@
+from sklearn.linear_model import LogisticRegression
+import argparse
+import os
+import numpy as np
+from sklearn.metrics import mean_squared_error
+import joblib
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import OneHotEncoder
+import pandas as pd
+from azureml.core.run import Run
+from azureml.core import Dataset
+
+ds = Dataset.Tabular.from_delimited_files(path=[(datastore, ('data/train.csv'))])
+
+
+# TODO: Split data into train and test sets.
+
+### YOUR CODE HERE ###a
+
+run = Run.get_context()
+
+def clean_data(data):
+    # Clean and one hot encode data
+    x_df = data.to_pandas_dataframe().dropna()
+    x_df.drop(["enrollee_id", "city"], axis=1, inplace=True)
+    gender = pd.get_dummies(x_df.gender, prefix="gender")
+    x_df.drop("gender", axis=1, inplace=True)
+    x_df.join(gender)
+
+    enrolled_university = pd.get_dummies(x_df.enrolled_university, prefix="enrolled")
+    x_df.drop("enrolled_university", axis=1, inplace=True)
+    x_df.join(enrolled_university)
+
+    education_level = pd.get_dummies(x_df.education_level, prefix="education_level")
+    x_df.drop("education_level", axis=1, inplace=True)
+    x_df.join(education_level)
+
+    major_discipline = pd.get_dummies(x_df.major_discipline, prefix="major_discipline")
+    x_df.drop("major_discipline", axis=1, inplace=True)
+    x_df.join(major_discipline)
+    
+    experience = pd.get_dummies(x_df.experience, prefix="experience")
+    x_df.drop("experience", axis=1, inplace=True)
+    x_df.join(experience)
+
+    company_size = pd.get_dummies(x_df.company_size, prefix="company_size")
+    x_df.drop("company_size", axis=1, inplace=True)
+    x_df.join(company_size)
+
+    company_type = pd.get_dummies(x_df.company_type, prefix="company_type")
+    x_df.drop("company_type", axis=1, inplace=True)
+    x_df.join(company_type)
+
+    last_new_job = pd.get_dummies(x_df.last_new_job, prefix="last_new_job")
+    x_df.drop("last_new_job", axis=1, inplace=True)
+    x_df.join(last_new_job)
+
+    y_df = x_df.pop("target")
+
+    return x_df, y_df
+    
+
+def main():
+    # Add arguments to script
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('--C', type=float, default=1.0, help="Inverse of regularization strength. Smaller values cause stronger regularization")
+    parser.add_argument('--max_iter', type=int, default=100, help="Maximum number of iterations to converge")
+
+    args = parser.parse_args()
+
+    run.log("Regularization Strength:", np.float(args.C))
+    run.log("Max iterations:", np.int(args.max_iter))
+    x, y = clean_data(ds)
+    x_train, x_test, y_train, y_test = train_test_split(x, y)
+    model = LogisticRegression(C=args.C, max_iter=args.max_iter).fit(x_train, y_train)
+
+    accuracy = model.score(x_test, y_test)
+    run.log("Accuracy", np.float(accuracy))
+
+    os.makedirs('outputs', exist_ok=True)
+    joblib.dump(model, 'outputs/project_model.joblib')
+
+if __name__ == '__main__':
+    main()
